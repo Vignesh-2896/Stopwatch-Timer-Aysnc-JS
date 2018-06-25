11d3be5db7a650f4f3c0bec349a1595164cd745a
Vignesh <vigneshb2896@gmail.com>
2018-06-25 21:34:52 +0530

    Add files via upload

---
+++ StopWatch.js
@@ -0,0 +1,57 @@
+var started = false;
+var timer = 0;
+var interval;
+function toggler()
+{
+	if(started == false)
+	{
+		started=true;
+		interval = setInterval(function()
+		{
+			timer = timer + 0.01;
+			document.getElementById("cur_time").innerHTML = timer.toFixed(2);
+		},10);
+	}
+	else if(started == true)
+	{
+		started = false;
+		clearInterval(interval);
+	}
+}
+function reset_time()
+{
+	started = false;
+	clearInterval(interval);	
+	timer =0.00;
+	document.getElementById("cur_time").innerHTML = timer;
+	
+	var past_element = document.getElementById("past_list")
+	while(past_element.firstChild)
+		past_element.removeChild(past_element.firstChild);
+}	
+function recorder()
+{
+	var li = document.createElement("li");
+	li.innerHTML = timer.toFixed(2);
+	document.getElementById("past_list").appendChild(li);
+}
+document.addEventListener('keypress',function(event)
+{
+	var key_val = event.key;
+	if(key_val == 's')
+	{
+		toggler();
+	}
+	else if(key_val == 'r')
+	{
+		reset_time();
+	}
+	else if(key_val == 't')
+	{
+		recorder();
+	}
+	else
+	{
+		alert("Please use the following keys : \n s - Start/Stop , r - Reset , t - Record Time");
+	}
+});
